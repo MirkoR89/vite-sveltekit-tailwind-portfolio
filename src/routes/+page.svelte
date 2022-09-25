7ff6522bcddb5f793f7abccc1192153682a0f190
MirkoR89 <mirko.rinelli@gmail.com>
2022-09-25 18:53:20 +0200

    chore: set palette colors nord theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-    class="h-screen flex justify-center items-center text-3xl font-mono bg-slate-800 text-gray-50"
+    class="h-screen flex justify-center items-center text-3xl font-mono bg-polarNight0 text-snowStorm0 dark:bg-snowStorm0 dark:text-polarNight0"
 >
     git init
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "polarNight0": "#2E3440",
+        "polarNight1": "#3B4252",
+        "polarNight2": "#434C5E",
+        "polarNight3": "#4C566A",
+        "snowStorm0": "#D8DEE9",
+        "snowStorm1": "#E5E9F0",
+        "snowStorm2": "#ECEFF4",
+        "frostGreen": "#8FBCBB",
+        "frostAqua": "#88C0D0",
+        "frostIce": "#81A1C1",
+        "frostBlue": "#5E81AC",
+        "auroraRed": "#BF616A",
+        "auroraOrange": "#D08770",
+        "auroraYellow": "#EBCB8B",
+        "auroraGreen": "#A3BE8C",
+        "auroraViolet": "#B48EAD"
+      },
+    },
   },
   plugins: [],
 }
