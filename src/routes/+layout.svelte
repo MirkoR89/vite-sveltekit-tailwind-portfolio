7a21397dbd8e8fe2a5d9a83c5e40024552f92bb3
MirkoR89 <mirko.rinelli@gmail.com>
2022-10-07 10:01:38 +0200

    chore: set routes and layout files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+</script>
+
+<nav
+    class="h-10 flex justify-end items-center gap-x-3 pr-3 text-xl font-mono bg-polarNight0 text-snowStorm0 dark:bg-snowStorm0 dark:text-polarNight0"
+>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <script>
     import "../app.css";
+
+    let test = 0;
+
+    const plusOne = () => {
+        test += 1;
+    };
 </script>
 
 <div
-    class="h-screen flex justify-center items-center text-3xl font-mono bg-polarNight0 text-snowStorm0 dark:bg-snowStorm0 dark:text-polarNight0"
+    class="h-screen flex flex-col justify-center items-center gap-y-3 text-3xl font-mono bg-polarNight0 text-snowStorm0 dark:bg-snowStorm0 dark:text-polarNight0"
 >
-    git init
+    <h1>Home</h1>
+    <span>{test}</span>
+    <button
+        class="bg-auroraGreen text-snowStorm0 px-2 py-1 w-fit rounded-xl"
+        on:click={plusOne}
+    >
+        Aggiungi 1
+    </button>
 </div>
