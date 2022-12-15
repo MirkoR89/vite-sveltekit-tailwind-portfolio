26930b4edcc6425339f69f8a32b7fd49b39e1723
MirkoR89 <mirko.rinelli@gmail.com>
2022-12-15 23:07:30 +0100

    fix: favicon and structure static folder

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <div
-    class="h-[calc(100vh-80px)] flex flex-col justify-center items-center gap-y-3 text-3xl font-mono dark:bg-polarNight0 dark:text-snowStorm0 bg-snowStorm2 text-polarNight0"
+    class="h-[calc(100vh-80px)] flex flex-col justify-center items-center gap-y-3 text-3xl dark:bg-polarNight0 dark:text-snowStorm0 bg-snowStorm2 text-polarNight0"
 >
     <h1>About</h1>
 </div>
